2c3a261d57b49c71f3e892801850b8e514d7c7dc
AJAY YADAV <58135870+ajay767@users.noreply.github.com>
2020-04-22 21:29:00 +0530

    managed code

--- app.js
+++ app.js
@@ -16,29 +16,43 @@ var Controller = (function () {
 
 var tracker = (function () {
 
-
     fetch("https://api.covid19india.org/data.json")
         .then(Response => Response.json())
         .then(data => {
-            var todayData = data.cases_time_series;
-            var todayDataUpdate = todayData[todayData.length - 1];
+            console.log(data);
+            var pastData = data.cases_time_series;
+            var pastDataUpdate = pastData[pastData.length - 1];
+
 
             var statedata = data.statewise;
             console.log(statedata);
+
+
+
+            var increasedCase = statedata[0].confirmed - pastDataUpdate.totalconfirmed;
+            document.querySelector('.card-1 span').innerHTML = increasedCase;
+
+            var increasedDeath = statedata[0].deaths - pastDataUpdate.totaldeceased;
+            document.querySelector('.card-2 span').innerHTML = increasedDeath;
+
+            var increasedRecovery= statedata[0].recovered - pastDataUpdate.totalrecovered;
+            document.querySelector('.card-3 span').innerHTML = increasedRecovery;
+
+
 
 
             document.querySelector('.card-1 p').innerHTML = parseInt(statedata[0].confirmed);
             document.querySelector('.card-2 p').innerHTML = parseInt(statedata[0].deaths);
             document.querySelector('.card-3 p').innerHTML = parseInt(statedata[0].recovered);
             document.querySelector('.card-head p').innerHTML = ('As per ' + statedata[0].lastupdatedtime + ' update given by Govt of India.');
-            document.querySelector('.card-4 p').innerHTML = parseInt(todayDataUpdate.dailyconfirmed);
+            document.querySelector('.card-4 p').innerHTML = parseInt(pastDataUpdate.dailyconfirmed);
             document.querySelector('.card-5 p').innerHTML = parseInt(statedata[0].active);
 
 
-            
-           
+
             for (var i = 1; i < statedata.length; i++) {
                 var current = statedata[i];
+
                 document.querySelector('.' + current.statecode + ' h2').innerHTML = current.state;
                 document.querySelector('.' + current.statecode + ' p .TC').innerHTML = current.confirmed;
                 document.querySelector('.' + current.statecode + ' p .TD').innerHTML = current.deaths;
